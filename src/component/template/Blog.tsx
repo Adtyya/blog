a4bc5856e12e62bbe59b44d571a74529e781a20a
Agata React 1 <aditya@evetehcsolution.com>
2023-04-02 14:41:36 +0700

    fix: build error

--- src/component/template/Blog.tsx
+++ src/component/template/Blog.tsx
@@ -3,7 +3,7 @@ import BlogCard from '../molecul/BlogCard';
 export default function Blog({ data }: { data: string[] }) {
   return (
     <div className="grid h-full w-full grid-cols-1 gap-x-8 gap-y-12 px-3 md:grid-cols-3 lg:px-0">
-      {data.map((el, i) => {
+      {data.map((_, i) => {
         return (
           <div key={i} className={i === 0 ? 'col-span-full' : ''}>
             <BlogCard index={i}></BlogCard>
